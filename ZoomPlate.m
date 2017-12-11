deaaaefed8c389014647d8c0f6020a265cd33aee
sameco <sameco501@gmail.com>
2017-12-11 17:40:52 +0100

    Update ZoomPlate.m

--- ZoomPlate.m
+++ ZoomPlate.m
@@ -1,11 +1,13 @@
 function [ F ] = ZoomPlate( img )
 
+%First of all we get the edges of the image which is already
+%bidimensional(black and white)
 BW = edge(img, 'canny',0.5);
-%figure;imshow(BW); title(‘BW CANNY’);
 [M,N] = size(BW)
 
 B = zeros(M,1)
 
+%Loop for every pixel
 for i=1:1:M
 aux=1;
 pixel = zeros(3,1);
@@ -13,6 +15,7 @@ for j=1:1:N
 pixel(aux)=BW(i,j);
 aux=aux+1;
 
+%We save in the B array the density of edges in every row
 if aux > 3
 aux = 1;
 if ( pixel(1) == 0 && pixel(2) == 0 && pixel(3) == 1)
@@ -25,11 +28,12 @@ pixel = zeros(3,1);
 end
 end
 end
-%figure;plot(B,M:-1:1);
+%We get the maximum values of the two columns of B
 [maxv,maxi]=max(B);
 Lindex = maxi-81;
 Hindex = maxi+81;
-Corte=[0 Lindex N Hindex]; %Determina coordenadas de corte
+%We determine the coordinates of the cut
+Corte=[0 Lindex N Hindex];
 F=imcrop(img,Corte);
-%figure;imshow(F);
+
 end
